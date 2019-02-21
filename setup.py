6c712527c074fb1b955bdd94f30fe8ff482cb354
D <d.g.mcindoe@gmail.com>
2019-02-21 22:09:08 +1300

    Tweaked package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fr:
 
 setuptools.setup(
 
-    name="emgee-core-dmacgit",
+    name="emgee_core",
     version="0.0.1",
     author="dMacGit",
     author_email="d.g.mcindoe@gmail.com",
